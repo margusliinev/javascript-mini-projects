e48db0d4d1c2d0fc63dd5920aa9427051e4274fa
Margus Liinev <margus.liinev@gmail.com>
2022-11-16 01:15:51 +0200

    Sidebar advanced functionality

--- 05-sidebar/main.js
+++ 05-sidebar/main.js
@@ -1,6 +1,8 @@
 const toggle = document.querySelector('.toggle');
 const closeBtn = document.querySelector('.close-btn');
+const sidebarLinks = document.querySelectorAll('.scroll-link');
 const sidebar = document.querySelector('.sidebar');
+const sections = document.querySelectorAll('section');
 
 toggle.addEventListener('click', function () {
     sidebar.classList.toggle('show-sidebar');
@@ -8,4 +10,40 @@ toggle.addEventListener('click', function () {
 
 closeBtn.addEventListener('click', function () {
     sidebar.classList.remove('show-sidebar');
+});
+
+sidebarLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        if (window.matchMedia('(max-width: 900px)').matches) {
+            sidebar.classList.remove('show-sidebar');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.matchMedia('(min-width: 1200px)').matches) {
+        sidebar.classList.add('show-sidebar');
+        sections.forEach(function (section) {
+            section.classList.add('fixed-sidebar');
+        });
+    } else {
+        sidebar.classList.remove('show-sidebar');
+        sections.forEach(function (section) {
+            section.classList.remove('fixed-sidebar');
+        });
+    }
+});
+
+window.matchMedia('(min-width: 1200px)').addEventListener('change', function () {
+    if (window.matchMedia('(min-width: 1200px)').matches) {
+        sidebar.classList.add('show-sidebar');
+        sections.forEach(function (section) {
+            section.classList.add('fixed-sidebar');
+        });
+    } else {
+        sidebar.classList.remove('show-sidebar');
+        sections.forEach(function (section) {
+            section.classList.remove('fixed-sidebar');
+        });
+    }
 });
