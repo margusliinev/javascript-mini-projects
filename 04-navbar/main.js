9f63b393c04d5146f2fa7a506f87178f99ad5960
Margus Liinev <margus.liinev@gmail.com>
2022-11-15 02:39:08 +0200

    media change event and small screen scroll fix

--- 04-navbar/main.js
+++ 04-navbar/main.js
@@ -11,9 +11,17 @@ btn.addEventListener('click', function () {
 scrollLinks.forEach(function (link) {
     link.addEventListener('click', function (e) {
         e.preventDefault();
+        const navHeight = nav.getBoundingClientRect().height;
+        if (navHeight > 82) {
+            links.classList.remove('show-links');
+        }
         const id = e.currentTarget.getAttribute('href');
         const section = document.querySelector(`${id}`);
         let position = section.offsetTop;
         window.scrollTo({ left: 0, top: position - 80 });
     });
+});
+
+window.matchMedia('(min-width: 900px)').addEventListener('change', function () {
+    links.classList.remove('show-links');
 });
