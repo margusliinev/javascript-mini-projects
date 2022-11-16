92a4f8b5a07513ff4b84f28b198b47caca3ec823
Margus Liinev <margus.liinev@gmail.com>
2022-11-16 19:25:44 +0200

    video pause notification

--- 08-video/main.js
+++ 08-video/main.js
@@ -1,11 +1,17 @@
 const switchBtn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
+const header = document.querySelector('header');
 
 switchBtn.addEventListener('click', function () {
     switchBtn.classList.toggle('slide');
     if (switchBtn.classList.contains('slide')) {
         video.pause();
+        const notification = document.createElement('h1');
+        notification.textContent = 'Paused';
+        header.append(notification);
     } else {
+        const notification = document.querySelector('header h1');
+        notification.remove();
         video.play();
     }
 });
