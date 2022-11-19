dfbae831b01c88a7cc155fa928939fad43dec6aa
Margus Liinev <margus.liinev@gmail.com>
2022-11-19 23:59:57 +0200

    filter food with buttons

--- 11-menu/main.js
+++ 11-menu/main.js
@@ -82,6 +82,7 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 function displayMenuItems(menuItems) {
     let displayMenu = menuItems.map(function (item) {
@@ -99,6 +100,22 @@ function displayMenuItems(menuItems) {
     displayMenu = displayMenu.join('');
     sectionCenter.innerHTML = displayMenu;
 }
+
+filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function (menuItem) {
+            if (menuItem.category === category) {
+                return menuItem;
+            }
+        });
+        if (category === 'all') {
+            displayMenuItems(menu);
+        } else {
+            displayMenuItems(menuCategory);
+        }
+    });
+});
 
 window.addEventListener('DOMContentLoaded', function () {
     displayMenuItems(menu);
