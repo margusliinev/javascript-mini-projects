93bb35468b215671ece2b2c811cc0acb87cb76f7
Margus Liinev <margus.liinev@gmail.com>
2022-12-04 22:35:48 +0200

    display buttons, set active class

--- 22-pagination/main.js
+++ 22-pagination/main.js
@@ -1,6 +1,10 @@
 const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
 const title = document.querySelector('.section-title h1');
 const container = document.querySelector('.container');
+const btnContainer = document.querySelector('.btn-container');
+
+let index = 0;
+let pages = [];
 
 async function fetchFollowers() {
     const response = await fetch(url);
@@ -23,7 +27,7 @@ function displayFollowers(data) {
 }
 
 function paginate(followers) {
-    const itemsPerPage = 10;
+    const itemsPerPage = 20;
     const numberOfPages = Math.ceil(followers.length / itemsPerPage);
     const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
         const start = index * itemsPerPage;
@@ -32,11 +36,25 @@ function paginate(followers) {
     return newFollowers;
 }
 
+function displayButtons(container, pages, activeIndex) {
+    let btns = pages.map((_, pageIndex) => {
+        return `<button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : null}" data-index="${pageIndex}">${pageIndex + 1}</button>`;
+    });
+    btns.push('<button class="next-btn"><i class="fas fa-chevron-right"></i></button>');
+    btns.unshift('<button class="prev-btn"><i class="fas fa-chevron-left"></i></button>');
+    container.innerHTML = btns.join('');
+}
+
+function setupUI() {
+    displayFollowers(pages[index]);
+    displayButtons(btnContainer, pages, index);
+}
+
 async function init() {
     const data = await fetchFollowers();
     title.textContent = 'Pagination';
-    displayFollowers(paginate(data)[0]);
-    const pages = paginate(data);
+    pages = paginate(data);
+    setupUI();
 }
 
 window.addEventListener('load', init);
